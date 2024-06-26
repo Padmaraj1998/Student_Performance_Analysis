183c577d47b718d2c7434c17b3f9cddcb6125c65
Padmaraj1998 <payanraj@gmail.com>
2024-06-26 14:15:32 +0530

    checking git

--- src/logger.py
+++ src/logger.py
@@ -4,7 +4,7 @@ from datetime import datetime
 
 
 LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%S')}.log"
-logs_path = os.path.join(os.getcwd(),"logs",LOG_FILE)
+logs_path = os.path.join(os.getcwd(),"logs")
 os.makedirs(logs_path,exist_ok=True)
 print(logs_path)
 LOG_FILE_PATH = os.path.join(logs_path,LOG_FILE)
@@ -16,5 +16,5 @@ logging.basicConfig(
     level=logging.INFO,
 )
 
-# if __name__=="__main__":
-#     logging.info("logging has started")
+if __name__=="__main__":
+    logging.info("logging has started")

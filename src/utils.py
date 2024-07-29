050f4711c5b5b23ab090411f6d8e0db691602b5b
Padmaraj1998 <payanraj@gmail.com>
2024-07-29 14:18:16 +0530

    estimator parameters

--- src/utils.py
+++ src/utils.py
@@ -4,6 +4,7 @@ import sys
 import numpy as np
 import pandas as pd
 
+from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import r2_score
 from src.exception import CustomException
 
@@ -20,14 +21,20 @@ def save_object(file_path,obj):
     except Exception as e:
         raise CustomException(e,sys)
     
-def evaluate_model(X_train,Y_train,X_test,Y_test,models):
+def evaluate_model(X_train,Y_train,X_test,Y_test,models,parameter):
     try:
         report = {}
         
         for i in range(len(list(models))):
             model = list(models.values())[i]
+            para=parameter[list(models.keys())[i]]
 
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,Y_train)
+
+            model.set_params(**gs.best_params_)
             model.fit(X_train,Y_train)
+            # model.fit(X_train,Y_train)
 
             Y_training_data_pred =  model.predict(X_train)
             Y_test_data_pred = model.predict(X_test)
